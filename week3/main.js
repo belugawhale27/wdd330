7037417b6667787d35fb0dc8fc4266645ed66ba1
belugawhale27 <ahdahlberg15@gmail.com>
2022-05-12 00:20:32 -0700

    reading and exercises for week 4

--- week3/main.js
+++ week3/main.js
@@ -101,7 +101,7 @@ game.start(quiz); */
 /*Chapter 6 and 7 Game Updates */
 
 //The questions and answers are being stored as objects inside an array
-const quiz = [
+/*const quiz = [
     { name: "Superman",realName: "Clark Kent"},
     { name: "Wonder Woman", realName: "Diana Prince"},
     { name:"Batman", realName: "Bruce Wayne"},
@@ -179,3 +179,62 @@ const game = {
 //still runs similarly to how it did after Chapter 6, but it updates at the end
 view.start.addEventListener('click', () => game.start(quiz), false);
 //Something isn't working right, I can't seem to figure it out . . .
+*/
+//From the CodePen
+const quiz = [
+    { name: "Superman",realName: "Clark Kent" },
+    { name: "Wonderwoman",realName: "Dianna Prince" },
+    { name: "Batman",realName: "Bruce Wayne" },
+  ];
+
+// View Object
+const view = {
+score: document.querySelector('#score strong'),
+question: document.getElementById('question'),
+result: document.getElementById('result'),
+info: document.getElementById('info'),
+render(target,content,attributes) {
+for(const key in attributes) {
+target.setAttribute(key, attributes[key]);
+}
+target.innerHTML = content;
+}
+};
+
+// Game Object
+const game = {
+start(quiz){
+this.score = 0;
+this.questions = [...quiz];
+// main game loop
+for(const question of this.questions){
+this.question = question;
+this.ask();
+}
+// end of main game loop
+this.gameOver();
+},
+ask(){
+const question = `What is ${this.question.name}'s real name?`;
+view.render(view.question,question);
+const response =  prompt(question);
+this.check(response);
+},
+check(response){
+const answer = this.question.realName;
+if(response === answer){
+view.render(view.result,'Correct!',{'class':'correct'});
+alert('Correct!');
+this.score++;
+view.render(view.score,this.score);
+} else {
+view.render(view.result,`Wrong! The correct answer was ${answer}`,{'class':'wrong'});
+alert(`Wrong! The correct answer was ${answer}`);
+}
+},
+gameOver(){
+view.render(view.info,`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
+}
+}
+
+game.start(quiz);
